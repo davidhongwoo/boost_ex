87d881746db0a908e2ad7b6c22faa076a9f38180
davidhongwoo <david.hongwoo.lee@gmail.com>
2015-06-29 22:16:22 +0900

    simplest bind sample.

---
+++ bind/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <boost/bind.hpp>
+
+using namespace std;
+
+double func(double a, double b) 
+{
+	return a - b;
+}
+
+int main() 
+{
+	double result = (boost::bind(&func, _1, _2))(1,0, 2.0);
+	cout << "a: " << result << endl;
+
+	result = (boost::bind(&func, _2, _1))(1,0, 2.0);
+	cout << "b: " << result << endl;
+
+	return 0;
+}
