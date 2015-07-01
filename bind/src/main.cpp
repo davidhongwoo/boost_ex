9a02d9958681e06062558f93c2fe0fe799402210
hongwoo <hongwoo7@gmail.com>
2015-07-01 20:00:11 +0900

    bind in bind.

--- bind/src/main.cpp
+++ bind/src/main.cpp
@@ -10,10 +10,13 @@ double func(double a, double b)
 
 int main() 
 {
-	double result = (boost::bind(&func, _1, _2))(1,0, 2.0);
+	double result = (boost::bind(&func, _1, _2))(1.0, 2.0);
 	cout << "a: " << result << endl;
 
-	result = (boost::bind(&func, _2, _1))(1,0, 2.0);
+	result = (boost::bind(&func, _2, _1))(1.0, 2.0);
+	cout << "b: " << result << endl;
+
+	result = (boost::bind(&func, _2, boost::bind(&func, _1, _2)))(1.0, 2.0);
 	cout << "b: " << result << endl;
 
 	return 0;
